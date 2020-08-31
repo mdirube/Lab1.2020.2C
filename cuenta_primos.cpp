0e8cc720783e1b2f9889324447e1975149d8e017
mdirube <67482531+mdirube@users.noreply.github.com>
2020-08-31 12:09:08 -0300

    Add files via upload

---
+++ cuenta_primos.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    int num, cantDiv=0, i=1 , ultimo=1, cantPrimos=0;
+    cout<<"NUMERO: ";
+    cin>>num;
+    while(ultimo<=num){
+        i=1;
+        cantDiv=0;
+        while(i<=ultimo){
+            if(ultimo%i==0){
+                    cantDiv++;
+            }
+            i++;
+        }
+        if(cantDiv==2){
+            cantPrimos++;
+        }
+        ultimo++;
+    }
+    cout<<"CANTIDAD DE PRIMOS: "<<cantPrimos<<endl;
+    system("pause");
+    return 0;
+}
