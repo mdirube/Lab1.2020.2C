b2c6165399c80810ccf2f6bcbbda7e4241274973
mdirube <67482531+mdirube@users.noreply.github.com>
2020-10-16 09:41:18 -0300

    Add files via upload
    Funciones clase 16/10

---
+++ funciones.h
@@ -0,0 +1,122 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+
+
+
+
+
+//int contarNumerosRepetidos(int v[], int tam , int numero);///cuenta la cantidad de veces que se repite un //número en el vector
+
+//int maximoVector(int v[], int tam);///devuelve  la posición (primera) que ocupa el máximo en el vector
+
+//int minimoVector(int v[], int tam);///devuelve  la posición (primera) que ocupa el mínimo en el vector
+
+void cargarAleatorio(int v[], int tam, int limite);///asigna valores aleatorios a cada al vector
+
+void ordenarVector(int v[], int tam );/// ordena de menor a mayor un vector
+
+int sumarVector(int v[], int tam ); /// suma los valores contenidos en el vector
+
+int copiarVector(int v[], int v2[],int tam ); ///copia el vector v en v2
+
+bool compararVectores(int v[], int v2[],int tam );// compara los dos vectores que recibe. Si son iguales ///devuelve true, si no devuelve false
+
+/// Desarrollo de funciones / Definiciones
+int posicionNumeroEnVector(int v[],int tam, int numero){
+    int i;
+    for(i=0;i<tam;i++){
+        if(v[i]==numero) return i;
+        }
+    return -1;
+}
+
+int minimoVector(int v[], int tam){
+    int i, posMin=0;
+    for(i=1;i<tam;i++){
+        if(v[i]<v[posMin]) {
+                posMin=i;
+        }
+    }
+    return posMin;
+}
+
+void ordenarVector(int v[], int tam ){
+    int i,j, posmin, aux;
+    for(i=0;i<tam-1;i++){
+        posmin=i;
+        for(j=i+1;j<tam;j++){
+            if(v[j]<v[posmin]) posmin=j;
+        }
+        aux=v[i];
+        v[i]=v[posmin];
+        v[posmin]=aux;
+    }
+}
+
+int maximoVector(int v[], int tam){
+    int i, posMax=0;
+    for(i=1;i<tam;i++){
+        if(v[i]>v[posMax]) {
+                posMax=i;
+        }
+    }
+    return posMax;
+}
+
+void cargarAleatorio(int v[], int tam, int limite){
+  int i;
+  srand(time(NULL));
+  for( i=0; i<tam; i++ ){
+        v[i]=(rand()%limite)+1;
+  }
+}
+
+void mostrarVector(int v[], int tam){
+    int i;
+    for(i=0;i<tam;i++){
+        cout<<v[i]<<"\t";
+    }
+}
+
+int contarNumerosRepetidos(int v[], int numero, int tam){
+    int i, cant=0;
+    for(i=0;i<tam;i++){
+        if(v[i]==numero) cant++;
+        }
+    return cant;
+}
+
+void ponerCero(int v[], int tam){
+    int i;
+    for(i=0;i<tam;i++){
+        v[i]=0;
+    }
+}
+
+int sumarVector(int v[], int tam ){
+    int i, suma=0;
+    for(i=0;i<tam;i++){
+        suma+=v[i];
+    }
+    return suma;
+}
+
+int copiarVector(int v[], int v2[],int tam ){
+    int i;
+    for(i=0;i<tam;i++){
+        v2[i]=v[i];
+    }
+}
+
+bool compararVectores(int v[], int v2[],int tam ){
+   int i;
+    for(i=0;i<tam;i++){
+        if(v2[i]!=v[i]) return false;
+    }
+    return true;
+}
+
+
+#endif // FUNCIONES_H_INCLUDED
+1
