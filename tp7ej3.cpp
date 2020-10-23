e65a6e0367e1a379f222b3a3832de4cb79a5e7c1
mdirube <67482531+mdirube@users.noreply.github.com>
2020-10-23 09:42:15 -0300

    Add files via upload

---
+++ tp7ej3.cpp
@@ -0,0 +1,30 @@
+/**3) Escribir una función que reciba tres números y retorne el promedio*/
+#include<iostream>
+using namespace std;
+#include<cstdlib>
+
+float  funcion(float ,float   ,float  );
+
+
+int main(){
+    float n1,n2,n3,resultado,promedio;
+    cout<<"ingrese un numero:"<<endl;
+    cin>>n1;
+    cout<<"ingrese un numero:"<<endl;
+    cin>>n2;
+    cout<<"ingrese un numero:"<<endl;
+    cin>>n3;
+    promedio=funcion(n1,n2,n3);
+    cout<<"PROMEDIO: ";
+    cout<<promedio<<endl;
+    system("pause");
+    return 0;
+}
+
+
+float  funcion(float a,float   b,float  c){
+    float suma,promedio;
+    suma=a+b+c;
+    promedio=suma/3;
+    return promedio;
+}
